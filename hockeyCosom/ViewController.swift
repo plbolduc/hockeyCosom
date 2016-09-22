257aeb6527af9ba2e92cafc3f65fb73b9fffcb0f
Tremblay, Marc-André <am36090@logti-a3412-04>
2016-09-22 12:08:44 -0400

    Ajout du Stepper

--- hockeyCosom/ViewController.swift
+++ hockeyCosom/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var stepper: UIStepper!
+    @IBOutlet var periode: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stepper.maximumValue = 3
+        stepper.minimumValue = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changePeriode(sender: UIStepper) {
+        self.periode.text = Int(sender.value).description
+    }
 
 }
 
