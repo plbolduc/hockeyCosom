21fd222a6bbd070947dd66ee27ff395aa1d656f1
Skeleton <am36370@logti-a3412-05>
2016-09-22 09:32:55 -0400

    Initial Commit

---
+++ hockeyCosom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hockeyCosom
+//
+//  Created by Bolduc, Pierre-Luc on 16-09-22.
+//  Copyright © 2016 Bolduc, Pierre-Luc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
