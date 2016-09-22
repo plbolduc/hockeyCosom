6fedae2f6a01b35eda9d12e7a6804df353f1ca8a
Frederic Fecteau <ffecteau@gmail.com>
2016-09-22 11:08:32 -0400

    Ajout de la classe Match

---
+++ hockeyCosom/Match.swift
@@ -0,0 +1,25 @@
+//
+//  Match.swift
+//  hockeyCosom
+//
+//  Created by Frederic Fecteau on 16-09-22.
+//  Copyright © 2016 Bolduc, Pierre-Luc. All rights reserved.
+//
+
+import Foundation
+
+class Match{
+
+    var Equipe = [String]();
+    
+    init ()
+    {
+        
+    }
+    
+    func AddPlayer(joueur : String) {
+        
+    }
+    
+}
+
