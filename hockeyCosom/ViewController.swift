aa4e428dbb18b4550959346f5affd2e91982e8ab
Frederic Fecteau <ffecteau@gmail.com>
2016-09-22 12:06:41 -0400

    Ajout des link des textbox

--- hockeyCosom/ViewController.swift
+++ hockeyCosom/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+        @IBOutlet weak var J1E1: UITextField!
+        @IBOutlet weak var J2E1: UITextField!
+        @IBOutlet weak var J3E1: UITextField!
+        @IBOutlet weak var J4E1: UITextField!
+        @IBOutlet weak var J5E1: UITextField!
+    
+        @IBOutlet weak var J1E2: UITextField!
+        @IBOutlet weak var J2E2: UITextField!
+        @IBOutlet weak var J3E2: UITextField!
+        @IBOutlet weak var J4E2: UITextField!
+        @IBOutlet weak var J5E2: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func StartGame(sender: UIButton) {
+//        let match:Match=Match()
+//        match.AddPlayerEquipe1(J1E1.text : String, numeroJoueur: <#T##Int#>)
+    }
 
 }
 
